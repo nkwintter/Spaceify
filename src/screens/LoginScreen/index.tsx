e83c4386530565ce04fb32c2785bddf61b3a7cab
nkwintter <nkwintter@gmail.com>
2025-07-01 23:12:24 -0300

    redirect funfando. Resolvi td(eu acho)

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -20,7 +20,7 @@ const LoginScreen = () => {
   React.useEffect(() => {
     if (isAuthenticated) {
       console.log('✅ Usuário autenticado, navegando para Home...');
-      navigation.replace('Home'); // Usar replace em vez de navigate
+      navigation.replace('Playlists'); // Usar replace em vez de navigate
     }
   }, [isAuthenticated, navigation]);
 
